f21d52612bd9b9e0ca6736f964496d31197b8a45
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2022-12-27 13:33:10 +0300

    Выделит логику точки маршрута в отдельный презентер

--- src/presenter/trips-presenter.js
+++ src/presenter/trips-presenter.js
@@ -1,8 +1,7 @@
-import {render, replace} from '../framework/render';
+import {render} from '../framework/render';
 import SortView from '../view/sort-view';
 import TripsListView from '../view/trips-list-view';
-import EventView from '../view/event-view';
-import EditEventFormView from '../view/edit-event-form-view';
+import EventPresenter from './event-presenter';
 import NoEventsView from '../view/no-events-view';
 
 export default class TripsPresenter {
@@ -27,58 +26,8 @@ export default class TripsPresenter {
   }
 
   #renderEvent(event, destinations, offersByType) {
-    const destination = destinations.find((item) => event.destination === item.id);
-    const availableOffers = offersByType.find((item) => item.type === event.type).offers;
-    const offers =
-      offersByType
-        .find((item) => item.type === event.type).offers
-        .filter((offer) => event.offers.includes(offer.id));
-
-    const escKeyDownHandler = (evt) => {
-      if (evt.key === 'Escape' || evt.key === 'Esc') {
-        evt.preventDefault();
-        replaceFormToCard();
-        document.removeEventListener('keydown', escKeyDownHandler);
-      }
-    };
-
-    const eventComponent = new EventView(
-      {
-        event,
-        destination,
-        offers,
-        onRollupButtonClick: () => {
-          replaceCardToForm.call(this);
-          document.addEventListener('keydown', escKeyDownHandler);
-        }
-      }
-    );
-
-    const eventEditComponent = new EditEventFormView(
-      {
-        event,
-        destination,
-        availableOffers,
-        isNewPoint: false,
-        onFormSubmit: closeEventEditForm,
-        onRollupButtonClick: closeEventEditForm
-      }
-    );
-
-    function replaceCardToForm() {
-      replace(eventEditComponent, eventComponent);
-    }
-
-    function replaceFormToCard() {
-      replace(eventComponent, eventEditComponent);
-    }
-
-    function closeEventEditForm() {
-      replaceFormToCard();
-      document.removeEventListener('keydown', escKeyDownHandler);
-    }
-
-    render(eventComponent, this.#tripsListComponent.element);
+    const eventPresenter = new EventPresenter(this.#tripsListComponent);
+    eventPresenter.init(event, destinations, offersByType);
   }
 
   #renderSort() {
